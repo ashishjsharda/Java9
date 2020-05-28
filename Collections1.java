97dcbfea42ba9609b6dea9d42154c97dbf23ec2f
Ashish S <ashishjsharda@gmail.com>
2020-05-27 21:45:12 -0400

    Create Collections1.java

---
+++ Collections1.java
@@ -0,0 +1,12 @@
+import java.util.Set;
+
+/**
+ * Using new collections
+ * @author ashish
+ */
+public class Collections1 {
+    public static void main(String[] args) {
+        Set<String>set=Set.of("Ron","Rick");
+        System.out.println(set);
+    }
+}

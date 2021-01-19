c324784783db2adb9caeaf22176ca45adb1031ff
Ashish S <ashishjsharda@gmail.com>
2021-01-19 06:25:59 -0500

    Create StringsEx.java

---
+++ StringsEx.java
@@ -0,0 +1,10 @@
+/**
+ * Java 9-Using Strings
+ * @author ashish
+ */
+public class StringsEx {
+    public static void main(String[] args) {
+        String names=String.join(",","Tom","Raj");
+        System.out.println(names);
+    }
+}
